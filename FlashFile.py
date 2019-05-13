04e86b53653da8ceab9dde621c8f52557de83bf0
Zeev Rotshtein <zeevro@gmail.com>
2019-05-13 18:02:47 +0300

    Conventions, white-spaces..

--- FlashFile.py
+++ FlashFile.py
@@ -2,67 +2,68 @@ import pprint
 import struct
 from ECC import *
 
-class FlashFile:	
-	def __init__(self,filename, page_size=0x200, oob_size=0x10, page_per_block=0x20):
-		self.DebugLevel=0
-		self.FileSize=0
-		self.UseAnsi=False
 
-		self.Open(filename)
-		self.SetPageInfo(page_size,oob_size,page_per_block)
-		
-	def IsInitialized(self):
-		return True
+class FlashFile:
+    def __init__(self, filename, page_size=0x200, oob_size=0x10, page_per_block=0x20):
+        self.DebugLevel = 0
+        self.FileSize = 0
+        self.UseAnsi = False
 
-	def SetUseAnsi(self,use_ansi):
-		self.UseAnsi=use_ansi
+        self.Open(filename)
+        self.SetPageInfo(page_size, oob_size, page_per_block)
 
-	def SetPageInfo(self, page_size, oob_size, page_per_block):
-		self.PageSize=page_size
-		self.OOBSize=oob_size
-		self.RawPageSize=self.PageSize+self.OOBSize
-		self.PagePerBlock=page_per_block
-		self.BlockSize=self.PageSize * self.PagePerBlock
-		self.RawBlockSize=self.RawPageSize * self.PagePerBlock
-		self.PageCount=(self.FileSize)/self.PageSize
-		self.BlockCount = self.PageCount/self.PagePerBlock
+    def IsInitialized(self):
+        return True
 
-		print 'PageSize: 0x%x' % self.PageSize
-		print 'OOBSize: 0x%x' % self.OOBSize
-		print 'PagePerBlock: 0x%x' % self.PagePerBlock
-		print 'BlockSize: 0x%x' % self.BlockSize
-		print 'RawPageSize: 0x%x' % self.RawPageSize
-		print 'PageCount: 0x%x' % self.PageCount
-		print 'FileSize: 0x%x' % self.FileSize
-		print ''
+    def SetUseAnsi(self, use_ansi):
+        self.UseAnsi = use_ansi
 
-	def Open(self,filename):
-		try:
-			self.fd=open(filename,'rb')
-			import os
-			self.FileSize=os.path.getsize(filename)
-		except:
-			print "Can't open a file:",filename
-			return False
-		return True
+    def SetPageInfo(self, page_size, oob_size, page_per_block):
+        self.PageSize = page_size
+        self.OOBSize = oob_size
+        self.RawPageSize = self.PageSize + self.OOBSize
+        self.PagePerBlock = page_per_block
+        self.BlockSize = self.PageSize * self.PagePerBlock
+        self.RawBlockSize = self.RawPageSize * self.PagePerBlock
+        self.PageCount = (self.FileSize) / self.PageSize
+        self.BlockCount = self.PageCount / self.PagePerBlock
 
-	def Close(self):
-		self.fd.close()
+        print('PageSize: 0x%x' % self.PageSize)
+        print('OOBSize: 0x%x' % self.OOBSize)
+        print('PagePerBlock: 0x%x' % self.PagePerBlock)
+        print('BlockSize: 0x%x' % self.BlockSize)
+        print('RawPageSize: 0x%x' % self.RawPageSize)
+        print('PageCount: 0x%x' % self.PageCount)
+        print('FileSize: 0x%x' % self.FileSize)
+        print()
 
-	def GetBlockOffset(self,block):
-		return block * self.RawBlockSize
+    def Open(self, filename):
+        try:
+            self.fd = open(filename, 'rb')
+            import os
+            self.FileSize = os.path.getsize(filename)
+        except:
+            print("Can't open a file:", filename)
+            return False
+        return True
 
-	def GetPageOffset(self,pageno):
-		return pageno*self.RawPageSize
+    def Close(self):
+        self.fd.close()
 
-	def ReadPage(self,pageno,remove_oob=False):
-		self.fd.seek(self.GetPageOffset(pageno))
+    def GetBlockOffset(self, block):
+        return block * self.RawBlockSize
 
-		if remove_oob:
-			return self.fd.read(self.PageSize)
-		else:
-			return self.fd.read(self.RawPageSize)
+    def GetPageOffset(self, pageno):
+        return pageno * self.RawPageSize
 
-	def ReadOOB(self,pageno):
-		self.fd.seek(pageno*self.RawPageSize+self.PageSize)
-		return self.fd.read(self.OOBSize)
+    def ReadPage(self, pageno, remove_oob=False):
+        self.fd.seek(self.GetPageOffset(pageno))
+
+        if remove_oob:
+            return self.fd.read(self.PageSize)
+        else:
+            return self.fd.read(self.RawPageSize)
+
+    def ReadOOB(self, pageno):
+        self.fd.seek(pageno * self.RawPageSize + self.PageSize)
+        return self.fd.read(self.OOBSize)
